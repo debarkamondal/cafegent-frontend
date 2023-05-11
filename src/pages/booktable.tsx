d0479ea7dde6101f04a607a2e946c74036f5f3c7
debarkamondal <debarkamondal@gmail.com>
2023-05-11 08:11:27 +0530

    changing logic so that a single qr scan is required

--- src/pages/booktable.tsx
+++ src/pages/booktable.tsx
@@ -2,8 +2,9 @@ import Qrcode from "@/components/Qrcode";
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/store";
-import { setTable, setPhone } from "@/redux/reservationSlice";
+import { setTable, setPhone, setName } from "@/redux/reservationSlice";
 import { useRouter } from "next/router";
+import BookTableHeader from "@/components/booktable/BookTableHeader";
 
 const BookTable = () => {
 	const host = process.env.NEXT_PUBLIC_BACKEND_URL;
@@ -14,20 +15,24 @@ const BookTable = () => {
 	const router = useRouter();
 	const [output, setoutput]: any = useState(); // using state to keep the output of the qrcode component streamlined for reuse
 
-	const handleUpdate = (event: any): void => {
+	const handlePhoneUpdate = (event: any): void => {
 		dispatch(setPhone(event.target.value));
+	};
+	const handleNameUpdate = (event: any): void => {
+		dispatch(setName(event.target.value));
 	};
 
 	const handleClick = async () => {
 		const url: string = `${host}:${port}/api/table/book/`;
 		const response = await fetch(url, {
-			method: "PUT",
+			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({
 				table: reservation.table,
 				phone: reservation.phone,
+				name: reservation.name,
 			}),
 		});
 		let data = await response.json();
@@ -51,47 +56,24 @@ const BookTable = () => {
 
 	return (
 		<>
-			<h1 className="mt-8 p-4 text-center text-4xl font-bold">Book a Table</h1>
-			<div className="flex m-auto p-4 justify-evenly w-96 ">
-				<div className="bg-green-300 border-4 rounded-full w-12 h-12 flex align-middle">
-					<div className="m-auto">1</div>
-				</div>
-				<hr className="border-t-2 border-dashed border-black w-20 mt-5" />
-				<div className="bg-green-300 border-4 rounded-full w-12 h-12 flex align-middle">
-					<div className="m-auto">2</div>
-				</div>
+			<BookTableHeader />
+			<h1 className="text-center m-4 ">
+				Please fill up while we are verifying QR code
+			</h1>
+			<div className="bookingInfo flex flex-col m-4">
+				<input
+					className="my-2"
+					type="text"
+					placeholder="Enter your name"
+					onChange={handleNameUpdate}
+				/>
+				<input
+					className="my-2"
+					type="text"
+					placeholder="Enter your Phone number"
+					onChange={handlePhoneUpdate}
+				/>
 			</div>
-			{!output ? (
-				<div className="m-auto mt-6 w-max">
-					<Qrcode
-						className="rounded-md"
-						fps={10}
-						qrbox={{ width: 250, height: 250 }}
-						disableFlip={false}
-						output={output}
-						setoutput={setoutput}
-					/>
-				</div>
-			) : null}
-			<div className="text-center flex flex-col justify-center h-60">
-				<h2 className="text-2xl mb-4">Step: {!output ? 1 : 2}</h2>
-				<p className="text-xl">
-					{!output
-						? "scan the QR code on the table to book"
-						: "Confirm table number "}
-				</p>
-			</div>
-			{output ? (
-				<div className="text-lg flex flex-col justify-between text-center">
-					Table : {JSON.parse(output).table}
-					<input
-						className="border-2 mt-6 m-auto w-10/12"
-						type="text"
-						onChange={handleUpdate}
-						placeholder="Enter Phone number"
-					/>
-				</div>
-			) : null}
 			<br />
 			{/* <div className="flex justify-center w-full"> */}
 			<button
--- src/redux/reservationSlice.tsx
+++ src/redux/reservationSlice.tsx
@@ -2,33 +2,36 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface ReservationState {
-  table: number;
-  orders: [];
-  phone: number;
-  availability: boolean;
+	table: number;
+	orders: [];
+	phone: number;
+	name: string;
 }
 
 const initialState: ReservationState = {
-  table: 0,
-  orders: [],
-  phone: 0,
-  availability: true,
+	table: 0,
+	orders: [],
+	phone: 0,
+	name: "",
 };
 
 export const reservationSlice = createSlice({
-  name: "reservation",
-  initialState,
-  reducers: {
-    setTable: (state, action: PayloadAction<number>) => {
-      state.table = action.payload;
-    },
-    setPhone: (state, action: PayloadAction<number>) => {
-      state.phone = action.payload;
-    },
-  },
+	name: "reservation",
+	initialState,
+	reducers: {
+		setTable: (state, action: PayloadAction<number>) => {
+			state.table = action.payload;
+		},
+		setPhone: (state, action: PayloadAction<number>) => {
+			state.phone = action.payload;
+		},
+		setName: (state, action: PayloadAction<string>) => {
+			state.name = action.payload;
+		},
+	},
 });
 
 // Action creators are generated for each case reducer function
-export const { setTable, setPhone } = reservationSlice.actions;
+export const { setTable, setPhone, setName } = reservationSlice.actions;
 
 export default reservationSlice.reducer;
