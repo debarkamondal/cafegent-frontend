a2febdb3e3617dd7d6f9a54dce783e0573112040
debarkamondal <debarkamondal@gmail.com>
2023-07-07 10:35:51 +0530

    checking table status upon page visit

--- src/app/booktable/[id]/page.tsx
+++ src/app/booktable/[id]/page.tsx
@@ -1,10 +1,9 @@
-"use client";
 import Button from "@/components/Button";
 import Footer from "@/components/Footer";
 import Greeter from "@/components/Greeter";
 import LoginForm from "@/components/LoginForm";
 import Providers from "@/redux/provider";
-import React from "react";
+import React, { Suspense } from "react";
 
 type Props = {};
 
@@ -13,6 +12,7 @@ const page = ({ params }: { params: { id: string } }) => {
 	params.id = params.id.replaceAll("%3A", ":");
 	console.log(params.id);
 	let res;
+
 	(async function tableStatus() {
 		const url = `${host}/table/check/${params.id}`;
 		const data = await fetch(url, { method: "GET" });
@@ -21,13 +21,14 @@ const page = ({ params }: { params: { id: string } }) => {
 	})();
 	return (
 		<Providers>
-			<div className="h-screen flex flex-col justify-center">
-				<Greeter />
-				<LoginForm />
-				<Button className="w-5/6 mx-auto mt-6" />
-				<p>{res}</p>
-			</div>
-			<Footer />
+			<Suspense fallback={<p>Loading...</p>}>
+				<div className="h-screen flex flex-col justify-center">
+					<Greeter />
+					<LoginForm />
+					<Button className="w-5/6 mx-auto mt-6" />
+				</div>
+				<Footer />
+			</Suspense>
 		</Providers>
 	);
 };
