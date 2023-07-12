5d32eda6645ce1cc385f6ae711340c5957d73969
debarkamondal <debarkamondal@gmail.com>
2023-07-12 19:56:01 +0530

    re: turned errorBanner into reusable banner component

--- src/app/booktable/[id]/page.tsx
+++ src/app/booktable/[id]/page.tsx
@@ -1,4 +1,4 @@
-import ErrorBanner from "@/components/ErrorBanner";
+import { Banner } from "@/components/Banner";
 import Footer from "@/components/Footer";
 import Greeter from "@/components/Greeter";
 import LoginForm from "@/components/LoginForm";
@@ -15,16 +15,11 @@ const fetchTableData = async (id: string) => {
 const page = async ({ params }: { params: { id: string } }) => {
 	const id = params.id.replaceAll("%3A", ":");
 	const res = await fetchTableData(params.id);
-	const color = res.type === "error" ? "blue" : "green";
 	return (
 		<Providers>
 			<Suspense fallback={<p>Loading...</p>}>
-				{res.message && (
-					<ErrorBanner
-						message={res.message}
-						type={res.type}
-						className={`absolute top-0 text-center m-4 p-2 bg-${color}-300 border border-${color}-600 text-${color}-900`}
-					/>
+				{res.type === "error" && (
+					<Banner message={res.message} type={res.type} variant={"error"} />
 				)}
 				<Greeter />
 				<LoginForm token={id} />
