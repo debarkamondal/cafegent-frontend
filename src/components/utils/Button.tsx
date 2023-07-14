12890111aba815f204a5601be2c2d5d1eb3b7b31
debarkamondal <debarkamondal@gmail.com>
2023-07-14 12:13:48 +0530

    minor design fix

--- src/components/utils/Button.tsx
+++ src/components/utils/Button.tsx
@@ -3,15 +3,15 @@ import React, { FC } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const buttonVariants = cva("rounded-xl m-auto text-lg text-primary-100", {
+const buttonVariants = cva("m-auto text-lg text-primary-100", {
 	variants: {
 		variant: {
 			default: "bg-primary-900 text-primary-300",
 			disabled: "bg-gray-400 text-white",
 		},
 		size: {
-			default: "flex justify-center py-2 h-12 w-5/6",
-			small: "h-6 w-16 text-xs",
+			default: "flex justify-center py-2 h-12 w-5/6 rounded-xl",
+			small: "h-6 w-16 text-xs rounded-md",
 		},
 		defaultVariants: {
 			variant: "default",
