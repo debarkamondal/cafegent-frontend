a9cd488dccdf0bb27be0269b8a4a5849204628a9
debarkamondal <debarkamondal@gmail.com>
2023-07-17 02:39:51 +0530

    switched to axios

--- src/app/booktable/[id]/page.tsx
+++ src/app/booktable/[id]/page.tsx
@@ -3,24 +3,25 @@ import Footer from "@/components/utils/Footer";
 import Greeter from "@/components/booktable/Greeter";
 import LoginForm from "@/components/booktable/LoginForm";
 import Providers from "@/redux/provider";
+import axios from "axios";
 
 const host = process.env.NEXT_PUBLIC_BACKEND_URL;
 const fetchTableData = async (id: string) => {
 	const url = `${host}/table/check/${id}`;
-	const data = await fetch(url, { method: "GET", next: { revalidate: 2 } });
-	return await data.json();
+	const res = await axios.get(url);
+	return res.data;
 };
 
 const page = async ({ params }: { params: { id: string } }) => {
 	const id = params.id.replaceAll("%3A", ":");
-	const res = await fetchTableData(params.id);
+	const data = await fetchTableData(params.id);
 	return (
 		<Providers>
-			{res.type === "error" && (
-				<Banner message={res.message} type={res.type} variant={"error"} />
+			{data.type === "error" && (
+				<Banner message={data.message} type={data.type} variant={"error"} />
 			)}
 			<Greeter />
-			<LoginForm token={id} type={res.type} />
+			<LoginForm token={id} type={data.type} />
 			<Footer className="fixed bottom-0" />
 		</Providers>
 	);
