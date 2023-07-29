29b9f3bf716678313e7357c0f8843d9cbdfcb08b
debarkamondal <debarkamondal@gmail.com>
2023-07-29 21:24:13 +0530

    fix: checkout button placement in menu page

--- src/components/menu/OrderButton.tsx
+++ src/components/menu/OrderButton.tsx
@@ -4,13 +4,14 @@ import { Button } from "../utils/Button";
 const OrderButton = () => {
 	return (
 		<>
-			<div className="fixed w-full bottom-0 h-16 bg-gradient-to-t backdrop-blur-[10px] mix-blend-darken from-primary-900 to-primary-100 pt-3 "></div>
-			<Button
-				message="Place Order"
-				variant={"default"}
-				size={"default"}
-				className="sticky bottom-4 "
-			/>
+			<div className="fixed w-full bottom-0 h-16 bg-gradient-to-t backdrop-blur-[10px] mix-blend-darken from-primary-900 to-primary-100 pt-3 ">
+				<Button
+					message="Place Order"
+					variant={"default"}
+					size={"default"}
+					className="relative bottom-4"
+				/>
+			</div>
 		</>
 	);
 };
